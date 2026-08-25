b7d8242bcd28121fc549f81c6f3309aec0b2ff66
archief21 <willy.zoete@skynet.be>
2026-08-25 10:17:52 +0200

    Add files via upload

---
+++ behoorlijk.js
@@ -0,0 +1,362 @@
+window.DATASET = [
+
+/* ==================================================
+   A = TAXONOMIE
+   ================================================== */
+
+{ terms:["Taxonomie van de algemene beginselen van behoorlijk bestuur"], code:"A" },
+
+{ terms:["Legaliteit en bevoegdheid"], code:"AA" },
+{ terms:["Legaliteitsbeginsel / wettigheidsbeginsel"], code:"AAA" },
+{ terms:["Bevoegdheidsbeginsel"], code:"AAB" },
+{ terms:["Verbod van machtsoverschrijding"], code:"AAC" },
+{ terms:["Verbod van machtsafwending"], code:"AAD" },
+
+{ terms:["Zorgvuldige voorbereiding"], code:"AB" },
+{ terms:["Zorgvuldigheidsbeginsel"], code:"ABA" },
+{ terms:["Onderzoeksbeginsel"], code:"ABB" },
+{ terms:["Correcte feitenvaststelling"], code:"ABC" },
+{ terms:["Hoorbeginsel / hoorplicht"], code:"ABD" },
+{ terms:["Informatieplicht"], code:"ABE" },
+
+/* Motivering volgens de structuur van de Uitdrukkelijke Motiveringswet */
+{ terms:["Motivering en transparantie"], code:"AC" },
+{ terms:["Motiveringsbeginsel"], code:"ACA" },
+{ terms:["Formele motiveringsplicht"], code:"ACAA" },
+{ terms:["Motieven uitdrukkelijk vermeld"], code:"ACAAA" },
+{ terms:["Motieven opgenomen in de akte"], code:"ACAAB" },
+{ terms:["Motieven kenbaar voor de bestuurde"], code:"ACAAC" },
+{ terms:["Materiële / inhoudelijke motivering"], code:"ACAB" },
+{ terms:["Juridische overwegingen"], code:"ACAC" },
+{ terms:["Toepasselijke rechtsregels"], code:"ACACA" },
+{ terms:["Juridische grondslag"], code:"ACACB" },
+{ terms:["Toepassing van de rechtsregel op het concrete geval"], code:"ACACC" },
+{ terms:["Feitelijke overwegingen"], code:"ACAD" },
+{ terms:["Concrete feitelijke gegevens"], code:"ACADA" },
+{ terms:["Relevante feiten"], code:"ACADB" },
+{ terms:["Juiste feiten"], code:"ACADC" },
+{ terms:["Feiten die de beslissing dragen"], code:"ACADD" },
+{ terms:["Afdoende motivering"], code:"ACAE" },
+{ terms:["Relevantie van de motieven"], code:"ACAEA" },
+{ terms:["Draagkracht van de motieven"], code:"ACAEB" },
+{ terms:["Geen vage of algemene motivering"], code:"ACAEC" },
+{ terms:["Geen tegenstrijdige motieven"], code:"ACAED" },
+{ terms:["Geen kennelijk onjuiste feitelijke motieven"], code:"ACAEE" },
+{ terms:["Transparantiebeginsel"], code:"ACB" },
+{ terms:["Kenbaarheidsbeginsel"], code:"ACC" },
+
+{ terms:["Gelijkheid en objectiviteit"], code:"AD" },
+{ terms:["Gelijkheidsbeginsel"], code:"ADA" },
+{ terms:["Discriminatieverbod"], code:"ADB" },
+{ terms:["Objectiviteitsbeginsel"], code:"ADC" },
+{ terms:["Onpartijdigheidsbeginsel"], code:"ADD" },
+{ terms:["Neutraliteitsbeginsel"], code:"ADE" },
+
+{ terms:["Redelijkheid en evenredigheid"], code:"AE" },
+{ terms:["Redelijkheidsbeginsel"], code:"AEA" },
+{ terms:["Evenredigheidsbeginsel / proportionaliteitsbeginsel"], code:"AEB" },
+{ terms:["Geschiktheid"], code:"AEBA" },
+{ terms:["Noodzakelijkheid"], code:"AEBB" },
+{ terms:["Evenredigheid stricto sensu"], code:"AEBC" },
+{ terms:["Verbod van willekeur"], code:"AEC" },
+
+{ terms:["Rechtszekerheid en vertrouwen"], code:"AF" },
+{ terms:["Rechtszekerheidsbeginsel"], code:"AFA" },
+{ terms:["Vertrouwensbeginsel"], code:"AFB" },
+{ terms:["Consistentiebeginsel"], code:"AFC" },
+{ terms:["Verbod van retroactieve benadeling"], code:"AFD" },
+
+{ terms:["Procedurele behoorlijkheid"], code:"AG" },
+{ terms:["Fair-playbeginsel"], code:"AGA" },
+{ terms:["Hoorbeginsel"], code:"AGB" },
+{ terms:["Redelijketermijnbeginsel"], code:"AGC" },
+{ terms:["Toegankelijkheid"], code:"AGD" },
+{ terms:["Transparantie in de procedure"], code:"AGE" },
+
+{ terms:["Billijkheid en behoorlijkheid"], code:"AH" },
+{ terms:["Billijkheidsbeginsel"], code:"AHA" },
+{ terms:["Beginsel van behoorlijk en redelijk bestuur"], code:"AHB" },
+
+{ terms:["Contextgebonden beginselen"], code:"AI" },
+{ terms:["Openbaarheidsbeginsel"], code:"AIA" },
+{ terms:["Beginsel van objectieve selectie"], code:"AIB" },
+{ terms:["Beginsel van mededinging"], code:"AIC" },
+{ terms:["Beginsel van behoorlijk financieel beheer"], code:"AID" },
+{ terms:["Continuïteitsbeginsel"], code:"AIE" },
+{ terms:["Beginsel van actieve dienstverlening"], code:"AIF" },
+{ terms:["Beginsel van actieve openbaarheid"], code:"AIG" },
+
+/* ==================================================
+   B = FUNCTIONELE INDELING
+   ================================================== */
+
+{ terms:["Functionele indeling van de beginselen"], code:"B" },
+
+{ terms:["Mag het bestuur dit doen?"], code:"BA" },
+{ terms:["Legaliteit"], code:"BAA" },
+{ terms:["Bevoegdheid"], code:"BAB" },
+{ terms:["Machtsoverschrijding"], code:"BAC" },
+{ terms:["Machtsafwending"], code:"BAD" },
+
+{ terms:["Heeft het bestuur het dossier behoorlijk onderzocht?"], code:"BB" },
+{ terms:["Zorgvuldigheid"], code:"BBA" },
+{ terms:["Onderzoek"], code:"BBB" },
+{ terms:["Feitenvaststelling"], code:"BBC" },
+{ terms:["Informatieverzameling"], code:"BBD" },
+{ terms:["Hoorrecht als onderdeel van zorgvuldige voorbereiding"], code:"BBE" },
+
+{ terms:["Is de procedure behoorlijk verlopen?"], code:"BC" },
+{ terms:["Fair play"], code:"BCA" },
+{ terms:["Hoorplicht"], code:"BCB" },
+{ terms:["Redelijke termijn"], code:"BCC" },
+{ terms:["Toegankelijkheid van de procedure"], code:"BCD" },
+{ terms:["Procedurele transparantie"], code:"BCE" },
+
+{ terms:["Klopt de feitelijke grondslag?"], code:"BD" },
+{ terms:["Correcte feiten"], code:"BDA" },
+{ terms:["Relevante feiten"], code:"BDB" },
+{ terms:["Volledige feitenvaststelling"], code:"BDC" },
+{ terms:["Actueel feitenmateriaal"], code:"BDD" },
+
+{ terms:["Kan het bestuur uitleggen waarom het dit heeft beslist?"], code:"BE" },
+{ terms:["Motiveringsbeginsel"], code:"BEA" },
+{ terms:["Formele motiveringsplicht"], code:"BEAA" },
+{ terms:["Materiële / inhoudelijke motivering"], code:"BEAB" },
+{ terms:["Juridische overwegingen"], code:"BEAC" },
+{ terms:["Feitelijke overwegingen"], code:"BEAD" },
+{ terms:["Afdoende motivering"], code:"BEAE" },
+{ terms:["Relevantie"], code:"BEAEA" },
+{ terms:["Draagkracht"], code:"BEAEB" },
+{ terms:["Geen vage of algemene motivering"], code:"BEAEC" },
+{ terms:["Geen tegenstrijdige motieven"], code:"BEAED" },
+{ terms:["Geen kennelijk onjuiste feitelijke motieven"], code:"BEAEE" },
+
+{ terms:["Heeft het bestuur iedereen objectief behandeld?"], code:"BF" },
+{ terms:["Gelijkheid"], code:"BFA" },
+{ terms:["Discriminatieverbod"], code:"BFB" },
+{ terms:["Objectiviteit"], code:"BFC" },
+{ terms:["Onpartijdigheid"], code:"BFD" },
+{ terms:["Neutraliteit"], code:"BFE" },
+{ terms:["Verbod van willekeur"], code:"BFF" },
+
+{ terms:["Is de beslissing inhoudelijk redelijk en evenredig?"], code:"BG" },
+{ terms:["Redelijkheid"], code:"BGA" },
+{ terms:["Evenredigheid / proportionaliteit"], code:"BGB" },
+{ terms:["Geschiktheid"], code:"BGBA" },
+{ terms:["Noodzakelijkheid"], code:"BGBB" },
+{ terms:["Evenredigheid stricto sensu"], code:"BGBC" },
+{ terms:["Billijkheid"], code:"BGC" },
+
+{ terms:["Zijn rechtszekerheid en vertrouwen gerespecteerd?"], code:"BH" },
+{ terms:["Rechtszekerheid"], code:"BHA" },
+{ terms:["Vertrouwen"], code:"BHB" },
+{ terms:["Consistentie"], code:"BHC" },
+{ terms:["Voorspelbaarheid"], code:"BHD" },
+{ terms:["Verbod van retroactieve benadeling"], code:"BHE" },
+
+{ terms:["Zijn bijzondere of contextgebonden beginselen gerespecteerd?"], code:"BI" },
+{ terms:["Openbaarheid"], code:"BIA" },
+{ terms:["Objectieve selectie"], code:"BIB" },
+{ terms:["Mededinging"], code:"BIC" },
+{ terms:["Behoorlijk financieel beheer"], code:"BID" },
+{ terms:["Continuïteit"], code:"BIE" },
+{ terms:["Actieve dienstverlening"], code:"BIF" },
+{ terms:["Actieve openbaarheid"], code:"BIG" },
+
+/* Extra functionele tak voor de situatie waarin een feitelijke praktijk
+   bestaat zonder duidelijk aanwijsbare formele beslissing */
+{ terms:["Is er een rechtsgeldige en kenbare basis voor de bestuurlijke praktijk?"], code:"BJ" },
+{ terms:["Bestaat er een formeel besluit of andere rechtsgrond?"], code:"BJA" },
+{ terms:["Is het bevoegde bestuursorgaan aangewezen?"], code:"BJB" },
+{ terms:["Is de bevoegdheidsuitoefening traceerbaar?"], code:"BJC" },
+{ terms:["Is de praktijk verenigbaar met rechtszekerheid en transparantie?"], code:"BJD" },
+{ terms:["Wordt een feitelijke praktijk ten onrechte voorgesteld als formeel vastgesteld?"], code:"BJE" },
+
+/* ==================================================
+   C = MOMENT VAN CONTROLE
+   ================================================== */
+
+{ terms:["Moment waarop het bestuursoptreden wordt gecontroleerd"], code:"C" },
+
+{ terms:["1. Bevoegdheid – vóór het bestuursoptreden"], code:"CA" },
+{ terms:["Mag het bestuur optreden?"], code:"CAA" },
+{ terms:["Legaliteit en wettelijkheid"], code:"CAB" },
+{ terms:["Bevoegdheid"], code:"CAC" },
+{ terms:["Doel van de bevoegdheid"], code:"CAD" },
+{ terms:["Verbod van machtsafwending"], code:"CAE" },
+
+{ terms:["2. Voorbereiding – vóór de beslissing"], code:"CB" },
+{ terms:["Zorgvuldig onderzoek"], code:"CBA" },
+{ terms:["Onderzoek van de relevante feiten"], code:"CBB" },
+{ terms:["Correcte feitenvaststelling"], code:"CBC" },
+{ terms:["Verzamelen van noodzakelijke informatie"], code:"CBD" },
+{ terms:["Hoorbeginsel / hoorplicht"], code:"CBE" },
+{ terms:["Belangenidentificatie"], code:"CBF" },
+
+{ terms:["3. Procedure – tijdens de behandeling"], code:"CC" },
+{ terms:["Fair play"], code:"CCA" },
+{ terms:["Hoorrecht"], code:"CCB" },
+{ terms:["Gelijke procedurele behandeling"], code:"CCC" },
+{ terms:["Redelijke termijn"], code:"CCD" },
+{ terms:["Transparantie"], code:"CCE" },
+{ terms:["Toegankelijkheid"], code:"CCF" },
+
+{ terms:["4. Feiten – waarop de beslissing wordt gebaseerd"], code:"CD" },
+{ terms:["Juistheid van de feiten"], code:"CDA" },
+{ terms:["Volledigheid van de feiten"], code:"CDB" },
+{ terms:["Relevantie van de feiten"], code:"CDC" },
+{ terms:["Actualiteit van de gegevens"], code:"CDD" },
+
+{ terms:["5. Motivering – bij en na de besluitvorming"], code:"CE" },
+{ terms:["Formele motiveringsplicht"], code:"CEA" },
+{ terms:["Motieven uitdrukkelijk vermeld"], code:"CEAA" },
+{ terms:["Motieven opgenomen in de akte"], code:"CEAB" },
+{ terms:["Motieven kenbaar voor de bestuurde"], code:"CEAC" },
+{ terms:["Juridische overwegingen"], code:"CEB" },
+{ terms:["Toepasselijke rechtsregels"], code:"CEBA" },
+{ terms:["Juridische grondslag"], code:"CEBB" },
+{ terms:["Toepassing van de rechtsregel op het concrete geval"], code:"CEBC" },
+{ terms:["Feitelijke overwegingen"], code:"CEC" },
+{ terms:["Precieze en Concrete feitelijke gegevens"], code:"CECA" },
+{ terms:["Relevante feiten"], code:"CECB" },
+{ terms:["De inhoudelijke juistheid van de overwegingen (redenen, motieven)"], code:"CECC" },
+{ terms:["Feiten dragen (verantwoorden) de beslissing ?"], code:"CECD" },
+{ terms:["Materiële / inhoudelijke motivering"], code:"CED" },
+{ terms:["Afdoendheid"], code:"CEDA" },
+{ terms:["Relevantie van de motieven"], code:"CEDB" },
+{ terms:["Draagkracht van de motieven - Verantwoorden de motieven de beslissing ?"], code:"CEDC" },
+{ terms:["Geen vage of algemene motivering"], code:"CEDD" },
+{ terms:["Geen tegenstrijdige motieven"], code:"CEDE" },
+{ terms:["Geen kennelijk onjuiste feitelijke motieven"], code:"CEDF" },
+
+{ terms:["6. Gelijkheid en objectiviteit – bij de beoordeling"], code:"CF" },
+{ terms:["Vergelijkbare gevallen"], code:"CFA" },
+{ terms:["Gelijke behandeling"], code:"CFB" },
+{ terms:["Gerechtvaardigd onderscheid"], code:"CFC" },
+{ terms:["Discriminatieverbod"], code:"CFD" },
+{ terms:["Objectiviteit"], code:"CFE" },
+{ terms:["Onpartijdigheid"], code:"CFF" },
+{ terms:["Verbod van willekeur"], code:"CFG" },
+
+{ terms:["7. Inhoud – de uiteindelijke beslissing"], code:"CG" },
+{ terms:["Redelijkheid"], code:"CGA" },
+{ terms:["Evenredigheid / proportionaliteit"], code:"CGB" },
+{ terms:["Geschiktheid"], code:"CGBA" },
+{ terms:["Noodzakelijkheid"], code:"CGBB" },
+{ terms:["Evenredigheid stricto sensu"], code:"CGBC" },
+{ terms:["Billijkheid"], code:"CGC" },
+
+{ terms:["8. Rechtspositie – gevolgen van het bestuursoptreden"], code:"CH" },
+{ terms:["Rechtszekerheid"], code:"CHA" },
+{ terms:["Vertrouwensbeginsel"], code:"CHB" },
+{ terms:["Consistentie"], code:"CHC" },
+{ terms:["Voorspelbaarheid"], code:"CHD" },
+{ terms:["Verbod van retroactieve benadeling"], code:"CHE" },
+{ terms:["Continuïteit"], code:"CHF" },
+
+{ terms:["9. Bestaan en kenbaarheid van het bestuursoptreden"], code:"CI" },
+{ terms:["Is er een formele beslissing of rechtsgrond?"], code:"CIA" },
+{ terms:["Bevoegd bestuursorgaan"], code:"CIB" },
+{ terms:["Kenbaarheid en traceerbaarheid"], code:"CIC" },
+{ terms:["Verenigbaarheid van de feitelijke praktijk met de rechtspositie"], code:"CID" },
+{ terms:["Geen schijn van een formeel besluit zonder formele grondslag"], code:"CIE" },
+
+{ terms:["10. Contextgebonden controle"], code:"CJ" },
+{ terms:["Openbaarheid"], code:"CJA" },
+{ terms:["Objectieve selectie"], code:"CJB" },
+{ terms:["Mededinging"], code:"CJC" },
+{ terms:["Behoorlijk financieel beheer"], code:"CJD" },
+{ terms:["Actieve dienstverlening"], code:"CJE" },
+{ terms:["Actieve openbaarheid"], code:"CJF" },
+
+/* ==================================================
+   D = BEGRIPPEN EN MIDDELEN
+   Alfabetisch register
+   ================================================== */
+
+{ terms:["Begrippen en middelen"], code:"D" },
+
+{ terms:["Afdoende motivering"], code:"D001" },
+{ terms:["Algemeen belang"], code:"D002" },
+{ terms:["Algemene beginselen van behoorlijk bestuur"], code:"D003" },
+{ terms:["Administratief beroep"], code:"D004" },
+{ terms:["Annulatieberoep"], code:"D005" },
+
+{ terms:["Bestuurshandeling"], code:"D006" },
+{ terms:["Bestuursbesluit"], code:"D007" },
+{ terms:["Bevoegdheid"], code:"D008" },
+{ terms:["Bevoegdheidsbeginsel"], code:"D009" },
+{ terms:["Billijkheidsbeginsel"], code:"D010" },
+
+{ terms:["Discriminatie"], code:"D011" },
+{ terms:["Discriminatieverbod"], code:"D012" },
+
+{ terms:["Evenredigheidsbeginsel"], code:"D013" },
+
+{ terms:["Fair-playbeginsel"], code:"D014" },
+{ terms:["Feitelijke grondslag"], code:"D015" },
+{ terms:["Feitelijke overwegingen"], code:"D016" },
+{ terms:["Formele motivering"], code:"D017" },
+
+{ terms:["Gedeeltelijke vernietiging"], code:"D018" },
+{ terms:["Gelijkheidsbeginsel"], code:"D019" },
+{ terms:["Gelijke behandeling"], code:"D020" },
+{ terms:["Gerechtvaardigd onderscheid"], code:"D021" },
+{ terms:["Gewekt vertrouwen"], code:"D022" },
+
+{ terms:["Hoorbeginsel"], code:"D023" },
+{ terms:["Hoorplicht"], code:"D024" },
+
+{ terms:["Informatieplicht"], code:"D025" },
+{ terms:["Inhoudelijke motivering"], code:"D026" },
+
+{ terms:["Juridische grondslag"], code:"D027" },
+{ terms:["Juridische overwegingen"], code:"D028" },
+
+{ terms:["Kenbaarheid"], code:"D029" },
+{ terms:["Kenbaarheidsbeginsel"], code:"D030" },
+
+{ terms:["Legaliteitsbeginsel"], code:"D031" },
+
+{ terms:["Materiële motivering"], code:"D032" },
+{ terms:["Machtsafwending"], code:"D033" },
+{ terms:["Machtsoverschrijding"], code:"D034" },
+{ terms:["Motief"], code:"D035" },
+{ terms:["Motiveringsbeginsel"], code:"D036" },
+{ terms:["Motiveringsplicht"], code:"D037" },
+
+{ terms:["Nietigverklaring"], code:"D038" },
+{ terms:["Non-discriminatiebeginsel"], code:"D039" },
+
+{ terms:["Objectiviteitsbeginsel"], code:"D040" },
+{ terms:["Onpartijdigheidsbeginsel"], code:"D041" },
+{ terms:["Onderzoeksbeginsel"], code:"D042" },
+
+{ terms:["Proportionaliteitsbeginsel"], code:"D043" },
+
+{ terms:["Redelijkheidsbeginsel"], code:"D044" },
+{ terms:["Redelijketermijnbeginsel"], code:"D045" },
+{ terms:["Rechtsgrond"], code:"D046" },
+{ terms:["Rechtsmiddel"], code:"D047" },
+{ terms:["Rechtszekerheidsbeginsel"], code:"D048" },
+{ terms:["Relevantie van de motieven"], code:"D049" },
+
+{ terms:["Schorsing"], code:"D050" },
+{ terms:["Schorsingsberoep"], code:"D051" },
+
+{ terms:["Transparantiebeginsel"], code:"D052" },
+
+{ terms:["Uitdrukkelijke motivering"], code:"D053" },
+{ terms:["Uitdrukkelijke motiveringswet"], code:"D054" },
+
+{ terms:["Verbod van machtsafwending"], code:"D055" },
+{ terms:["Verbod van machtsoverschrijding"], code:"D056" },
+{ terms:["Verbod van willekeur"], code:"D057" },
+{ terms:["Vernietiging"], code:"D058" },
+{ terms:["Vertrouwensbeginsel"], code:"D059" },
+
+{ terms:["Wettigheidsbeginsel"], code:"D060" },
+
+{ terms:["Zorgvuldigheidsbeginsel"], code:"D061" }
+
+];
